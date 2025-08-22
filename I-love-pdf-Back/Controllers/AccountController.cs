3d9f76819f9d0382814fb796f083157c4ecdfea6
Abdelrhmancs <ght7234@gmail.com>
2025-08-22 22:29:21 +0300

    fix backend folder (remove submodule and add as normal folder)

---
+++ I-love-pdf-Back/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using I_love_pdf.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using I_love_pdf.Dto;
+
+namespace I_love_pdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        public readonly UserManager<ApplicationUser> useManager;
+        public readonly IConfiguration config;
+        public AccountController(UserManager<ApplicationUser> useManager, IConfiguration config)
+        {
+            this.useManager = useManager;
+            this.config = config;   
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto userDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = userDto.Email,
+                    Email = userDto.Email,
+                    Name = userDto.Name
+                };
+                var res = await useManager.CreateAsync(user, userDto.Password);
+                if (!res.Succeeded)
+                {
+                    return BadRequest(res.Errors);
+                }
+                return Ok("Registeration successfully");
+
+            }
+        }
+    }
+}
---
+++ I-love-pdf-Back/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace I_love_pdf.Models
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+       
+
+    }
+}
