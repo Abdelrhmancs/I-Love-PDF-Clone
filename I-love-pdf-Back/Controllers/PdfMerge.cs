c24cf30dedb35cd50a005834c902eeb4c08478a0
Abdelrhmancs <ght7234@gmail.com>
2025-08-25 00:57:47 +0300

    create merge backend

---
+++ I-love-pdf-Back/Controllers/PdfMerge.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using PdfSharpCore.Pdf;
+using PdfSharpCore.Pdf.IO;
+
+namespace I_love_pdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfMerge : ControllerBase
+    {
+        [HttpPost("merge")]
+        public async Task<IActionResult> Merge(List<IFormFile> files)
+        {
+            if(files != null && files.Count != 0)
+            {
+                PdfDocument outFile = new PdfDocument();
+                foreach(IFormFile file in files)
+                {
+                    var stream =  file.OpenReadStream();
+                    PdfDocument inputFile = PdfReader.Open(stream , PdfDocumentOpenMode.Import);
+                    foreach(var page in inputFile.Pages)
+                    {
+                        outFile.AddPage(page);
+                    }
+                }
+                var ms = new MemoryStream();
+                outFile.Save(ms, false);
+                ms.Position = 0;
+                return File(ms.ToArray(), "application/pdf", "merged.pdf");
+            }
+            return BadRequest("no psdf file uploaded");
+        }
+        
+    }
+}
