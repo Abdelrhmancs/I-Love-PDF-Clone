438dc69b92cb3b608afc2842e14e124c6e16a09c
Abdelrhmancs <ght7234@gmail.com>
2025-08-25 08:35:00 +0300

    remove hover

--- I-love-pdf-front/src/components/Card.jsx
+++ I-love-pdf-front/src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ icon, title, text ,route }) => {
     <>
       <div onClick = {()=> navigate(route)} className=" group mt-3 transtion duration-500 border hover:cursor-pointer hover:border-black bg-white rounded-xl shadow-md p-5 flex w-[240px] h-[240px] flex-col">
         <div className=" flex  rounded-lg ">
-            <img src={icon} className="w-[50px] group-hover:ms-[65px] transtion duration-500" alt="" />
+            <img src={icon} className="w-[50px] transtion duration-500" alt="" />
             </div>
         <div className="flex flex-col  "> 
           <h3 className="mt-4 w-full text-[22px] font-semibold text-[#33333b]">{title}</h3>
