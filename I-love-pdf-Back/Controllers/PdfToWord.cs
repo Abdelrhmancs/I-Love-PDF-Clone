99a89c2a5b900886a739664e984959643738ad9b
Abdelrhmancs <ght7234@gmail.com>
2025-08-26 03:09:48 +0300

    create pdf to word backend

---
+++ I-love-pdf-Back/Controllers/PdfToWord.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spire.Pdf;
+using System.IO;
+using System.Threading.Tasks;
+namespace I_love_pdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PdfToWord : ControllerBase
+    {
+
+        [HttpPost("convert-to-word")]
+        public async Task<IActionResult> ConvertToWord(List<IFormFile> files)
+        {
+            if (files == null || files.Count == 0)
+                return BadRequest("Please upload at least one PDF file.");
+
+            var file = files.First(); 
+
+            using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            var pdf = new Spire.Pdf.PdfDocument();
+            pdf.LoadFromStream(memoryStream);
+
+            using var outputStream = new MemoryStream();
+            pdf.SaveToStream(outputStream, Spire.Pdf.FileFormat.DOCX);
+            outputStream.Position = 0;
+
+            return File(
+                outputStream.ToArray(),
+                "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                Path.GetFileNameWithoutExtension(file.FileName) + ".docx"
+            );
+        }
+
+    }
+}
