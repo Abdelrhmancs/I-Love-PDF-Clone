9fb0c503ab32c96b0bf666db4ed8580ff5f2c65f
Abdelrhmancs <ght7234@gmail.com>
2025-09-09 10:54:08 +0300

    create watermark backend (Calling script)

---
+++ I-love-pdf-Back/Controllers/Watermark.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace I_love_pdf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Watermark : ControllerBase
+    {
+        [HttpPost("Watermark")]
+        public async Task<IActionResult> WatermarkPdf(List<IFormFile> files,  string text)
+        {
+            if (files == null || files.Count == 0)
+            {
+                return BadRequest("upload file");
+            }
+            else if (text == "")
+            {
+                return BadRequest("write a watermark text");
+            }
+            var file = files.First();
+            var pythonScript = @"D:\Backend\I-Love-PDF-Clone\Pdf-Scripts";
+            var uploadFolder = Path.Combine(Directory.GetCurrentDirectory(), "uploadFiles");
+            if (!Directory.Exists(uploadFolder))
+            {
+                Directory.CreateDirectory(uploadFolder);
+            }
+
+            var inputFile = Path.Combine(uploadFolder, file.FileName);
+            var outputFile = Path.Combine(uploadFolder, "addWatermark_" + file.FileName);
+
+            var stream = new FileStream(inputFile, FileMode.Create);
+            await file.CopyToAsync(stream);
+            stream.Close();
+
+
+            //script
+            var filepath = @"D:\Backend\I-Love-PDF-Clone\Pdf-Scripts\Watermark.py";
+            var psi = new ProcessStartInfo
+            {
+                FileName = "Python",
+                Arguments = $"\"{filepath}\" \"{inputFile}\" \"{outputFile}\" \"{text}\"",
+                WorkingDirectory = pythonScript,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (var process = Process.Start(psi))
+            {
+                await process.WaitForExitAsync();
+                var error = process.StandardError.ReadToEnd();
+                if (process.ExitCode != 0)
+                {
+                    return StatusCode(500, "faild :" + error);
+                }
+
+            }
+
+            var FileBaytes = await System.IO.File.ReadAllBytesAsync(outputFile);
+
+            return File(FileBaytes, "application/pdf", Path.GetFileName(outputFile));
+
+
+
+
+
+        }
+    }
+}
